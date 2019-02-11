01ace22c6cb07ab15d5c0694f7b6d1ae3e6ce837
Julian Anderson <julian@myboost.id>
2019-02-11 11:14:21 +0700

    initial files and folders

---
+++ server.js
@@ -0,0 +1,33 @@
+//require modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const next = require('next');
+const enrouten = require('express-enrouten');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+//getting database configuration
+require('./config/db');
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(bodyParser.json());
+
+  //enrouten into controllers directory
+  server.use(enrouten({
+    directory: 'controllers'
+  }));
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
